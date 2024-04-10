1a02a5495bd4200b7ec2a830db0dea136f7ced41
Kairav21 <naickerkd@gmail.com>
2024-04-10 10:39:27 +0200

    Added UI for project

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -28,13 +28,91 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblName = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lbldragonName = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.rtbLog = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(37, 34);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(86, 16);
+            this.lblName.TabIndex = 0;
+            this.lblName.Text = "Player Name";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 79);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(781, 48);
+            this.textBox1.TabIndex = 1;
+            // 
+            // lbldragonName
+            // 
+            this.lbldragonName.AutoSize = true;
+            this.lbldragonName.Location = new System.Drawing.Point(37, 157);
+            this.lbldragonName.Name = "lbldragonName";
+            this.lbldragonName.Size = new System.Drawing.Size(92, 16);
+            this.lbldragonName.TabIndex = 2;
+            this.lbldragonName.Text = "Dragon Name";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(40, 207);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(781, 60);
+            this.textBox2.TabIndex = 3;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(40, 304);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(781, 62);
+            this.btnSave.TabIndex = 4;
+            this.btnSave.Text = "Save";
+            this.btnSave.UseVisualStyleBackColor = true;
+            // 
+            // rtbLog
+            // 
+            this.rtbLog.Location = new System.Drawing.Point(40, 412);
+            this.rtbLog.Name = "rtbLog";
+            this.rtbLog.Size = new System.Drawing.Size(781, 96);
+            this.rtbLog.TabIndex = 5;
+            this.rtbLog.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(921, 520);
+            this.Controls.Add(this.rtbLog);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.lbldragonName);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lbldragonName;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.RichTextBox rtbLog;
     }
 }
 
